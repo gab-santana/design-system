c03ec440c166dac271c59eaa930e0c99c2d35e68
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-07-31 22:42:20 -0300

    add box, heading and text stories

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit id quam magnam saepe voluptas tenetur sit vel nisi repellendus deleniti tempore ipsum ea commodi temporibus hic, nesciunt et maxime iusto!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
